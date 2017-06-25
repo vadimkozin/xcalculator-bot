47ecf84ea399c4ce3b1b08812941c09d31822d7e
Vadim Kozin <vadim@tkrss.ru>
2017-06-25 16:38:57 +0300

    +

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 require('dotenv').load();
 require('./src/bot');
-require('./src/web');
+//require('./src/web');   // чтобы heroku не ругался вот так: Error R10 (Boot timeout) -> Web process failed to bind to $PORT within 60 seconds of launch
