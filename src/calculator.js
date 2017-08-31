c7c726d8fb40d5139d20df115a843b847161b5da
Vadim Kozin <vadim@tkrss.ru>
2017-08-31 15:36:16 +0300

    fixed bug with calculation

--- src/calculator.js
+++ src/calculator.js
@@ -6,17 +6,20 @@
  * ограничения: отрицательные числа в формуле не обрабатыватся: '12/-2' не сработает!
  * но если заменить знак отрицания, например на ! и сказать об этом в конструкторе, то работает '12/!2 = -6'
  * used:
- * let formula = '100 / 20 +3';
+ * let formula = '100 / 20 + 3';
  * let calc = new Calculator (formula);
  * console.log(formula, '=>', calc.result());
  */
-const log = require('./log')(module);
+//const log = require('./log')(module);
+
+function contains(arr, elem) {
+    return arr.indexOf(elem) != -1;
+}
 
 class Calculator  {
     constructor(formula, sign_denial=null) {
         this.formula = formula;     // исходная формула типа '10+5*100-25'
         this.sign_denial = sign_denial // заменитель знака отрицания, например !
-        this.tmp = this.formula;    // рабочая строка
         this.solution = 0;          // решение формулы
         
         this.methods = {            // операции калькулятора
@@ -35,38 +38,44 @@ class Calculator  {
     // подготовка: вокруг операций (** * / + -) слева и справа делаем один пробел
     _prepare() {
         
-        // удалим все пробелы
-        this.tmp = this.tmp.replace(/\s+/g, '');
-        // возведение в степень (**) заменим на Q
-        this.tmp = this.tmp.replace(/([*]{2})/g, 'Q');
-        // вокруг всех операций делаем оди пробел
-        this.tmp = this.tmp.replace(/([*/+-])/g, ' $1 ');
-        // Q меняем на возведение в спепень (**)
-        this.tmp = this.tmp.replace(/([Q])/g, ' ** ');        
+        this.formula = this.formula
+            .replace(/\s+/g, '')            // удалим все пробелы
+            .replace(/([*]{2})/g, 'Q')      // возведение в степень (**) заменим на Q
+            .replace(/([*/+-])/g, ' $1 ')   // вокруг всех операций cделаем один пробел
+            .replace(/([Q])/g, ' ** ');     // Q меняем на возведение в спепень (**)
     }
 
     // вычисление: ожидается строка типа: '10 + 5 * 100 - 4 / 2'   
     _calculate() {
-        const operators = ['**', '*', '/', '-', '+'];
+
+        // операции с учётом приоритета
+        const operators = [['**'], ['*', '/'], ['-', '+']];
+        
         const filler = '#';
 
-        let abc = this.tmp.split(' ');
+        let abc = this.formula.split(' ');
+
         if (this.sign_denial) {
             abc = abc.map(x => x.replace(this.sign_denial, '-'));
         }
 
         operators.forEach(op => {
             abc.forEach((v,i,a) => {
-                if (v === op) {
+                
+                if (contains(op, v)) {
+                    
                     a[i+1] = this.methods[v](a[i-1], a[i+1]);
                     a[i-1] = a[i] = filler;
                 }
+             
             });
+
             abc = abc.filter(v =>  v !== filler);
+
         });
 
-        this.solution = abc[0]; // должен остаться только один
-
+        this.solution = abc[0]; // 'должен остаться только один'
+        
     }
     result() {
         return this.solution;
